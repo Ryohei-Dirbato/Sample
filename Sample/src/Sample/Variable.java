435d9ec921fc4d0885aff570bab3c06334f098f1
Ryohei-Dirbato <ryohei.nose.x536@dirbato.co.jp>
2025-02-20 19:23:09 +0900

    commit 20250220-1

---
+++ Sample/src/Sample/Variable.java
@@ -0,0 +1,17 @@
+package Sample;
+
+public class Variable {
+
+    public static void main(String[] args) {
+        var number1 = 1;
+        number1 = 2;
+        number1 = number1 * 2;
+        System.out.println(number1);
+        
+        var str1 ="abc";
+        System.out.println(str1);
+        
+
+    }
+
+}
